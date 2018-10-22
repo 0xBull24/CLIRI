b7799506c4bd5d8a20d2fae5b89fbe13c5dcb54d
D.Biles <darron.biles@gmail.com>
2018-10-22 09:13:30 -0400

    spotify working

--- assets/js/liri.js
+++ assets/js/liri.js
@@ -2,16 +2,14 @@
 const result = require("dotenv").config();
 const inquirer = require('inquirer');
 const axios = require('axios');
-require('./spotify.js');
+const Spotify = require('node-spotify-api');
+const keys = require('./spotify.js');
+const spotify = new Spotify(keys.spotify);
+
 
 if (result.error) {
     throw result.error
 }
-
-// How to use the env files and the properties
-console.log(` The env file is the following: `)
-console.log(Spotify.id);
-console.log(Spotify.secret);
 
 // Menu for the App
 inquirer.prompt([
@@ -48,6 +46,66 @@ inquirer.prompt([
     console.log(`Ok, ${answers.menuChoice}`)
 
     // Switch statement based on selected answer
+    switch (answers.menuChoice) {
+        case 'Search for a song`s info (spotify-this-song)':
+            inquirer.prompt([
+                // Get info for the song to be searched for
+                {
+                    type: 'input',
+                    name: 'spotifysong',
+                    message: 'What song would you like to search for ?'
+                }
+            ]).then(response => {
+                spotifySearch(response.spotifysong)
+            })
+            break;
+        case 'Search for a movie`s info (movie-this)':
+
+            break;
+        case 'Search for a concert`s info':
+
+            break;
+        case 'Do what it says':
+
+            break;
+    }
 }).catch(error => {
     console.log(error)
 })
+
+function spotifySearch(song) {
+
+    if (!song) {
+        song = 'The Sign'
+    }
+
+    spotify
+        .search({
+            type: 'track',
+            query: song,
+            limit: 3
+        })
+        .then(function (response) {
+            // console.log(response.tracks);
+            let tracks = response.tracks.items;
+            // console.log(tracks)
+            tracks.forEach(element => {
+                song = {}
+
+                // Traverse artist array
+                element.artists.forEach(element => {
+                    song.artists = element.name
+                });
+
+                song.album_name = element.album.name
+                song.songtitle = element.name
+                song.urlpreview = element.external_urls.spotify
+
+                // Do something with the object
+                console.log(song)
+            });
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+}
--- assets/js/spotify.js
+++ assets/js/spotify.js
@@ -1,8 +1,6 @@
-// Keys
-exports.Spotify = Spotify = {
+
+exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET,
 };
-
-
 
